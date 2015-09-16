a2b30150740e81c725911dad6c706501909d4a0c
liyayun <lyyaun@163.com>
2015-09-16 21:24:23 +0800

    coverage

--- tdd/main.js
+++ tdd/main.js
@@ -1,3 +1,10 @@
+// 斐波那契数列
+// 当 n === 0 时，返回 0；n === 1时，返回 1;
+// n > 1 时，返回 fibonacci(n) === fibonacci(n-1) + fibonacci(n-2)，如 fibonacci(10) === 55;
+// n 不可大于10，否则抛错，因为 Node.js 的计算性能没那么强。
+// n 也不可小于 0，否则抛错，因为没意义。
+// n 不为数字时，抛错。
+
 var fibonacci = function (n) {
   if (typeof n !== 'number') {
     throw new Error('n should be a Number');
